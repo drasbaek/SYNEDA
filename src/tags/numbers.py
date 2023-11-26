c2c4ba29b323e1b86be5baa79518464eef43dba0
MinaAlmasi <mina.almasi00@gmail.com>
2023-11-26 10:05:51 +0100

    init numbers py

---
+++ src/tags/numbers.py
@@ -0,0 +1,16 @@
+'''
+Generate number lists
+'''
+
+def cardinal():
+    '''
+    Generate cardinal numbers with random numbers
+    ''' 
+    pass 
+
+def date():
+    '''
+    Generate dates with different formats
+    '''
+    pass
+
