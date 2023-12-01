70e2da5aa6d3a331c4aef141a99e29d9162fd416
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-01 10:37:57 +0100

    rename to avoid deps conflicts

--- src/tags/number.py
+++ src/tags/number.py
@@ -3,10 +3,25 @@ Generate number lists
 
 NB. Note that it can both be numbers (e.g., 1-100) and words (e.g., en-ti)
 '''
-import sys
 import pathlib
-sys.path.append(str(pathlib.Path(__file__).parents[2] / "src"))
-from utils import load_data
+import pandas as pd
+
+def load_number_type(data_path, entity_type="MONEY"):
+    df = pd.read_excel(data_path / "LISTS.xlsx", sheet_name=entity_type)
+
+    return df
+
+def load_data(data_path, entity_types=["EVENT", "FACILITY", "GPE", "LANGUAGE", "LAW", "LOCATION", "MONEY", "NORP", "ORDINAL", "ORGANIZATION", "PRODUCT", "QUANTITY", "TIME", "WORK OF ART"]):
+    # initialize df for all entities
+    df_all = pd.DataFrame()
+
+    # load data for all entities
+    for type in entity_types:
+        df = pd.read_excel(data_path / "LISTS.xlsx", sheet_name=type)
+        df["TYPE"] = type
+        df_all = pd.concat([df_all, df], ignore_index=True)
+
+    return df_all
 
 def cardinal():
     '''
@@ -46,7 +61,7 @@ def money(currencies:list):
 def main():
     # define paths 
     path = pathlib.Path(__file__)
-    data_path = path.parents[1] / "data"
+    data_path = path.parents[2] / "data"
 
     # load data
     df = load_data(data_path, entity_types=["MONEY"])
