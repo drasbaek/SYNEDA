342d0fba2d9c63570b2150265f982adb931b00c6
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-01 13:39:21 +0100

    rm old money fn

--- src/tags/money.py
+++ src/tags/money.py
@@ -43,92 +43,6 @@ def money(data_path):
     numbers_words = number_words()
 
     # combine numbers and words
-    numbers = np.concatenate([small_numbers, big_numbers, numbers_words])
-
-    # duplicate entities based on weight
-    df = df.loc[df.index.repeat(df["weight"])].reset_index(drop=True)
-
-    # define string invalid currencies, all currencies that have single quantiy as NO in df + ",-"
-    string_invalid_currencies = df[df["only_single_quantity"] == "YES"]["entity"].tolist()
-    string_invalid_currencies.extend([",-", "$", "€", "£"])
-
-    # filter out invalid currencies
-    df_string_valid =  df[~df["entity"].isin(string_invalid_currencies)]
-
-    formatted_numbers = []
-
-    for num in numbers: 
-        # exclude the previously sampled indices
-        sampled_indices = []
-        available_indices = [i for i in range(len(df["entity"])) if i not in sampled_indices]
-    
-        if len(available_indices) > 0:
-            # Sample randomly from available indices
-            sampled_index = np.random.choice(available_indices)
-            currency = df["entity"].iloc[sampled_index]
-            
-            # Append the sampled index to the list
-            sampled_indices.append(sampled_index)
-            
-            # Use sampled_currency as needed
-        else:
-            # Handle the case where all rows have been sampled
-            print("All rows have been sampled.")
-
-        # define accepted formats
-        after_format = f"{num} {currency}"
-        before_format = f"{currency} {num}"
-        after_no_space_format = f"{num}{currency}"
-        before_no_space_format = f"{currency}{num}"
-        
-        currency_idx = df[df["entity"] == currency].index[0]
-
-        # extract placement and single quantity
-        placement = df.loc[currency_idx, "placement"]
-        single_quantity = df.loc[currency_idx, "only_single_quantity"]
-
-        # check if single quantity is YES, update number
-        if single_quantity == "YES":
-            # update num
-            num = np.random.choice(["en", 1])
-
-        # if placement is both, update placement to before or after based on random choice 
-        if placement == 'both':
-            placement = np.random.choice(["before", "after"])   
-
-        if num in numbers_words:
-            if currency in string_invalid_currencies: # replace invalid currency with random valid currency
-                currency = np.random.choice(df_string_valid["entity"])
-                
-            formatted_num = after_format     
-
-        # if num is int
-        elif num not in numbers_words: 
-            if placement == "before":
-                formatted_num = np.random.choice([before_format, before_no_space_format])
-            elif placement == "after":
-                formatted_num = np.random.choice([after_format, after_no_space_format])
-
-        # append to list of formatted numbers
-        formatted_numbers.append(formatted_num)
-
-    return formatted_numbers
-
-def money_new(data_path): 
-    '''
-    Generate money with different currencies from list. 
-    
-    (Note that we need to account for all kinds of weird formatting e.g., 1,000.00 kr and 200DKK, 200 DKK, 200 kr. 200kr.)
-    '''
-    # read in data
-    df = pd.read_excel(data_path / "CLEAN_LISTS.xlsx", sheet_name="MONEY")
-
-    # generate random numbers (100)
-    small_numbers = np.random.randint(1, 150, size=100)
-    big_numbers = np.random.randint(1000, 10000, size=50)
-    numbers_words = number_words()
-
-    # combine numbers and words
     all_numbers = np.concatenate([small_numbers, big_numbers, numbers_words])
     numeric_numbers = np.concatenate([small_numbers, big_numbers])
 
@@ -195,7 +109,7 @@ def main():
     data_path = path.parents[2] / "data"
 
     # load data
-    formatted_numbers = money_new(data_path)
+    formatted_numbers = money(data_path)
 
     print(formatted_numbers)
 
