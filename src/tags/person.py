c5ffb21d911af356683c145122f8c7cc8639bf4f
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-02 21:58:13 +0100

    add docstring + add check_for_duplicates skeleton

--- src/tags/person.py
+++ src/tags/person.py
@@ -6,7 +6,11 @@ import pandas as pd
 import numpy as np
 
 # create the tags for the PERSON entity
-def txt_to_df(path):
+def txt_to_df(path: pathlib.Path):
+    '''
+    Load a txt file into a dataframe
+    '''
+
     df = pd.read_fwf(path, encoding="latin-1", header=None, sep = "\t")
 
     # split the data into columns
@@ -14,7 +18,19 @@ def txt_to_df(path):
 
     return df
 
-def preprocess_person_df(df, remove_first_two_rows=True):
+def preprocess_person_df(df:pd.DataFrame, remove_first_two_rows:bool=True):
+    '''
+    Preprocess the person df from txt file. 
+    Optionally remove the first two rows (first name dataframes have two rows of metadata which the last name dataframe does not have).
+
+    Args
+        df: dataframe with names and amount of times they appear
+        remove_first_two_rows: whether to remove the first two rows or not
+
+    Returns
+        new_df: preprocessed dataframe with capitalized names and colnames in English
+    '''
+
     # remove first two row 
     df = df.iloc[2:]
 
@@ -32,33 +48,25 @@ def preprocess_person_df(df, remove_first_two_rows=True):
 
     return new_df
 
-def all_normal_persons(data_path, df_names: list=["first_names_women_2023.txt", "first_names_men_2023.txt", "last_names_2023.txt"]): 
-    '''
-    Pipeline for loading and preprocessing all the normal persons lists
-
-    Args:
-        data_path (str): path to the data folder with the three normal persons lists
-    '''
-    # load the three lists
-    dfs = [txt_to_df(data_path / name) for name in df_names]
-
-    # preprocess the dfs, but do it differently for the last names
-    women_df, men_df = [preprocess_person_df(df, remove_first_two_rows=True) for df in dfs[:2]]
-    last_names = preprocess_person_df(dfs[2], remove_first_two_rows=False)
-
-    return men_df, women_df, last_names
-
-def sample_person_tags(person_df, n_names_total=400, high=2000, low=10, ratio=(0.75, 0.25)):
+def sample_person_tags(person_df:pd.DataFrame, n_names_total:int=200, high:int=2000, low:int=10, ratio:tuple=(0.75, 0.25)):
     '''
     Sample PERSON tags from the list of names. Account for the amount of times the name appears in df (sampling more frequent names more often).
 
-    Args:
-        person_df (pd.DataFrame): dataframe with names and amount of times they appear
-        n_names_total (int): total number of names to sample
+    Args
+        person_df: dataframe with names and amount of times they appear
+        n_names_total: total number of names to sample (common + rare)
         high: upper bound for amount of times a name has to appear to be considered common
         low: lower bound for amount of times a name has to appear to be considered rare
-        ratio (tuple): ratio of common names to rare names (common, rare)
+        ratio: ratio of common names to rare names (common, rare). Values must sum to 1.
+    
+    Returns
+        sampled_names: dataframe with sampled names
+        info_before, info_after: info about the length of the pools before and after sampling
     '''
+
+    # raise value error if ratio does not sum to 1
+    if sum(ratio) != 1:
+        raise ValueError("Ratio must sum to 1")
 
     # sample names
     common_names = person_df.loc[person_df["amount"] >= high]
@@ -67,6 +75,7 @@ def sample_person_tags(person_df, n_names_total=400, high=2000, low=10, ratio=(0
     # info 
     info_before = f"Length of pools BEFORE sampling:\nCommon names: {len(common_names)}, Rare names: {len(rare_names)}"
 
+    # sample names from both pools 
     common_names = common_names.sample(int(n_names_total*ratio[0]), replace=False)
     rare_names = rare_names.sample(int(n_names_total*ratio[1]), replace=False)
 
@@ -81,6 +90,28 @@ def sample_person_tags(person_df, n_names_total=400, high=2000, low=10, ratio=(0
 
     return sampled_names, info_before, info_after
 
+def all_normal_persons(data_path, df_names: list=["first_names_women_2023.txt", "first_names_men_2023.txt", "last_names_2023.txt"]): 
+    '''
+    Pipeline for loading and preprocessing all the normal persons lists
+
+    Args:
+        data_path: path to the data folder with the three "normal" persons lists
+    '''
+    # load the three lists
+    dfs = [txt_to_df(data_path / name) for name in df_names]
+
+    # preprocess the dfs, but do it differently for the last names
+    women_df, men_df = [preprocess_person_df(df, remove_first_two_rows=True) for df in dfs[:2]]
+    last_names = preprocess_person_df(dfs[2], remove_first_two_rows=False)
+
+    return men_df, women_df, last_names
+
+def check_for_duplicates(dfs):
+    '''
+    Check for duplicates between the three name lists
+    '''
+    pass
+
 def main():
     path = pathlib.Path(__file__)
     data_path = path.parents[2] / "lists" / "names" 
@@ -89,7 +120,7 @@ def main():
     men_df, women_df, last_names = all_normal_persons(data_path)
 
     # sample names
-    men_df, info_before, info_after = sample_person_tags(men_df, 200, high=2000, low=10, ratio=(0.75, 0.25))
+    men_df, info_before, info_after = sample_person_tags(men_df, n_names_total=200, high=2000, low=100, ratio=(0.75, 0.25))
 
     # print all names where amount is 1
     print(men_df)
