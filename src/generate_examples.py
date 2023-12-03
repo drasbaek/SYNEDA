da29750ce2c006114d5c6b7d3cd2bdb92d209808
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-03 16:02:54 +0100

    add new sampling

--- src/generate_examples.py
+++ src/generate_examples.py
@@ -1,20 +1,61 @@
 import pathlib
 import pandas as pd
 import numpy as np
+import random
 
-def load_data(data_path, entity_types=["EVENT", "FACILITY", "GPE", "LANGUAGE", "LAW", "LOCATION", "MONEY", "NORP", "ORDINAL", "ORGANIZATION", "PRODUCT", "QUANTITY", "TIME", "WORK OF ART"]):
+def load_data(data_path):
+    # complete entities
+    coded_entity_types = ["DATE", "MONEY", "PERCENT", "QUANTITY", "PERSON"]
+
+    # manual entities
+    manual_entity_types = ["EVENT", "FACILITY", "GPE", "LANGUAGE", "LAW", "LOCATION", "NORP", "ORDINAL", "ORGANIZATION", "PRODUCT", "TIME", "WORK OF ART", "CARDINAL"]
+
     # initialize df for all entities
     df_all = pd.DataFrame()
-
-    # load data for all entities
-    for type in entity_types:
-        df = pd.read_excel(data_path / "LISTS.xlsx", sheet_name=type)
-        df["TYPE"] = type
+    
+    # load data for entities that are manual (not coded)
+    for ent_type in manual_entity_types:
+        df = pd.read_excel(data_path / "CLEAN_LISTS.xlsx", sheet_name=ent_type)
+        df["TYPE"] = ent_type
         df_all = pd.concat([df_all, df], ignore_index=True)
+    
+    # load data for entities that are coded
+    for ent_type in coded_entity_types:
+        df = pd.read_csv(data_path / f"{ent_type}.csv")
+        df["TYPE"] = ent_type
+        df_all = pd.concat([df_all, df], ignore_index=True)
+
+    # load multiples
+    multiple = pd.read_excel(data_path / "CLEAN_LISTS.xlsx", sheet_name="MULTIPLE")
+    multiple["TYPE"] = "MULTIPLE"
+    
+    # add multiples to df_all but only the entity col 
+    df_all = pd.concat([df_all, multiple[["entity", "weight", "context", "TYPE"]]], ignore_index=True)
+
+    return df_all, multiple
+
+def expand_data(df_all):
+    """
+    Expands data by multiplying rows by the weight of each entity.
+    """
+    
+    # multiply rows by weight
+    df_all = df_all.loc[df_all.index.repeat(df_all["weight"])].reset_index(drop=True)
+
+    # drop weights
+    df_all = df_all.drop(columns=["weight"])
 
     return df_all
 
-def create_examples(df_all, n_examples, n_entities=[1,3]):
+def sample_number():
+    # Define the probabilities for each number
+    probabilities = np.array([0.5, 0.3, 0.15, 0.05])
+    numbers = np.array([1, 2, 3, 4])
+
+    # Use np.random.choice to sample based on the specified probabilities
+    return np.random.choice(numbers, p=probabilities)
+
+def create_examples(df_all, n_examples):
     """
     Creates lists of entity examples (such as: [WORK OF ART: "One Dance", EVENT: "Folketingsvalget 2019"]) to generate synthetic text from.
 
@@ -26,23 +67,29 @@ def create_examples(df_all, n_examples, n_entities=[1,3]):
     Returns:
         examples (list): List of examples.
     """
-
+    
     # initialize list of examples
     examples = []
+
+    # shuffle all rows in df_all
+    df_all = df_all.sample(frac=1, random_state=1209).reset_index(drop=True)
 
     # create examples
     for i in range(n_examples):
         # sample number of entities
-        n = np.random.randint(n_entities[0], n_entities[1]+1)
-
-        # sample entities
-        entities = df_all.sample(n=n)
+        n_entities = sample_number()
+        
+        # subset df_all to only include n_entities
+        df_subset = df_all.sample(n_entities, replace=False).reset_index(drop=True)
 
         # create example
         example = []
-        for index, row in entities.iterrows():
-            example.append(f"{row['TYPE']}: {row['entity']}")
-
+        for index, row in df_subset.iterrows():
+            if row["context"] is np.nan:
+                example.append(f"{row['TYPE']}: {row['entity']}")
+            else:
+                example.append(f"{row['TYPE']}: {row['entity']} {{{row['context']}}}")
+    
         # append example to list of examples
         examples.append(example)
 
@@ -50,20 +97,27 @@ def create_examples(df_all, n_examples, n_entities=[1,3]):
 
 
 def main():
+    # set seed
+    np.random.seed(1209)
+
     # define paths 
     path = pathlib.Path(__file__)
     data_path = path.parents[1] / "data"
 
     # load data
-    df = load_data(data_path, entity_types=["EVENT", "FACILITY", "GPE", "LANGUAGE", "LAW", "LOCATION", "NORP", "ORDINAL", "ORGANIZATION", "PRODUCT", "TIME", "WORK OF ART"])
+    df, multiple = load_data(data_path)
+
+    # multiply rows by weight
+    df = expand_data(df)
+
+    # save df to check 
+    df.to_csv(data_path / "test.csv", index=False)
 
     # create examples
-    examples = create_examples(df, n_examples=50, n_entities=[1,3])
+    examples = create_examples(df, n_examples=50)
 
     for i, example in enumerate(examples):
         print(f"{i+1}. {example}")
-
-    
 
 if __name__ == "__main__":
     main()
