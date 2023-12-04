265c79bdb95125779a61e33f01611c525190b477
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-04 13:03:57 +0100

    rename main to ent_summary

--- src/entity_distributions.py
+++ src/entity_distributions.py
@@ -1,15 +1,7 @@
 import pathlib
 import pandas as pd
 
-
-def main():
-    # define paths 
-    path = pathlib.Path(__file__)
-    data_path = path.parents[1] / "data"
-
-    # load entities
-    ent_df = pd.read_csv(data_path / "final_ents" / "NER_ENTS_OVERVIEW.csv")
-
+def ent_summary(ent_df):
     # summarize count of each label and save to a variable
     ent_counts = ent_df["TYPE"].value_counts()
 
@@ -29,7 +21,28 @@ def main():
     # rename columns
     ent_summary.columns = ["Count", "Percentage", "Unique Count", "Unique Percentage"]
 
-    print(ent_summary)
+    return ent_summary
+
+def plot_entity_count_barplot(ent_examples):
+    pass
+
+
+def main():
+    # define paths 
+    path = pathlib.Path(__file__)
+    data_path = path.parents[1] / "data"
+
+    # load entity dataframe
+    ent_df = pd.read_csv(data_path / "final_ents" / "NER_ENTS_OVERVIEW.csv")
+    #ent_examples = pd.read_csv(data_path / "final_ents" / "NER_EXAMPLES.csv")
+
+    # summarize entities
+    ent_summary_df = ent_summary(ent_df)
+    print(ent_summary_df)
+
+
+
+    
 
 
 if __name__ == "__main__":
