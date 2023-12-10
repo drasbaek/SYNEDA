eb6ca21cec2d30dd77c33e8955b07cadae011b29
Anton Drasbæk <202008161@post.au.dk>
2023-12-10 12:30:53 +0100

    added much formatting towards final form

--- src/true_labels.py
+++ src/true_labels.py
@@ -4,33 +4,168 @@ Make true labels for data
 import pathlib
 import pandas as pd
 import spacy
+import re
 
-def test():
-    nlp = spacy.load("da_core_news_sm")
-    doc = nlp("Nina Bang: var en fremtrædende figur i politik.")
+def convert_to_doc(df):
+    '''
+    Converts the sentences column to a spacy doc object.
 
-    span = doc[0:2]
+    Parameters
+        df (pandas.DataFrame): dataframe with sentences column
 
-    print(span.text)
-    print(span.start_char)
-    print(span.end_char)
-    print(span.id)
+    Returns
+        df (pandas.DataFrame): dataframe with sentences column changed to spacy doc object
+    '''
+    print("Loading blank spaCy model...")
+    nlp = spacy.blank("da")
 
+    df['doc'] = df['sentences'].apply(lambda x: nlp(x))
+
+    return df
+
+def create_sents(df):
+    '''
+    Create sents column for dataframe with a spacy doc column
+    '''
+
+    sents = []
+
+    for i in range(len(df)):
+        # get doc object
+        doc = df['doc'][i]
+        
+        # create sentences
+        sent = [{"start": 0, "end": len(doc.text)}]
+
+        # append to list
+        sents.append(sent)
+    
+    # add to dataframe
+    df['sents'] = sents
+
+    return df
+
+def create_tokens(df):
+    '''
+    Create tokens column for dataframe with a spacy doc column
+    '''
+    all_tokens = []
+
+    for i in range(len(df)):
+        # get doc object
+        doc = df['doc'][i]
+
+        # create tokens
+        tokens = [{"id": idx, "start": token.idx, "end": token.idx + len(token.text)} for idx, token in enumerate(doc)]
+
+        # append to list
+        all_tokens.append(tokens)
+
+    # add to dataframe
+    df['tokens'] = all_tokens
+
+    return df
+
+def remove_context(df):
+    '''
+    Removes context from the entities column that is within {}
+    '''
+
+    # remove curly brackets and their contents in entities column and white space infront of it
+    df['entities'] = df['entities'].str.replace(r'\s*{.*}\s*', '', regex=True)
+
+    return df  
+
+def create_entity_dict(df):
+    '''
+    Changes the formatting of entity lists from strings to a list of dictionaries.
+
+    Parameters
+       df (pandas.DataFrame): dataframe with entities column
+    
+    Returns
+        df (pandas.DataFrame): dataframe with entities column changed to list of dictionaries
+    '''
+    
+    # make into a list of dictionaries for each entity pair.
+    df['entities_dict'] = df['entities'].apply(lambda x: [{'ent': item.split(': ')[1], 'label': item.split(': ')[0]} for item in eval(x)])
+
+    return df
+
+def create_ents(df):
+    '''
+    Create ents column for dataframe with a spacy doc column
+    '''
+
+    # create list of all ents 
+    all_ents = []
+
+    for i, row in df.iterrows():
+        # get ents
+        ents_dict_list = row['entities_dict']
+
+        ents_in_row = []
+
+        for ents_dict in ents_dict_list:
+            # get ent
+            ent = ents_dict['ent']
+
+            # get label
+            label = ents_dict['label']
+
+            # check that ent is in doc in try except 
+            '''
+            if ent not in row['sentences']:
+                raise ValueError(f"Entity {ent} not in sentence {row['sentences']}, row {i}")
+            '''
+
+            # get start and end
+            start = row["sentences"].find(ent)
+            end = start + len(ent)
+
+            # create ent
+            ent = {"start": start, "end": end, "label": label}
+
+            # append to list
+            ents_in_row.append(ent)
+        
+        # append to list
+        all_ents.append(ents_in_row)
+
+    # add to dataframe
+    df['ents'] = all_ents
+    
+    return df
+            
 def main(): 
     # define paths
     path = pathlib.Path(__file__)
     data_path = path.parents[1] / "data"
 
     # load data
-    df = pd.read_excel(data_path / "final_ents" / "NER_EXAMPLES.xlsx", index_col=0)
+    df = pd.read_excel(data_path / "DATASET.xlsx", sheet_name="ENTS")
+    
+    # remove context from ents
+    df = remove_context(df)
 
-    # make into entities 
-    df['entities'] = df['entities'].apply(lambda x: {item.split(': ')[0]: item.split(': ')[1] for item in eval(x)})
+    # create entity dict
+    df = create_entity_dict(df)
 
-    # print first row 
-    print(df['entities'][0].keys())
+    # convert sentences to doc objects
+    df = convert_to_doc(df)
 
+    # create sents
+    df = create_sents(df)
+
+    # create tokens
+    df = create_tokens(df)
+
+    # create ents
+    df = create_ents(df)
+
+    # print row 27
+    print(df.iloc[27])
 
 
 if __name__ == "__main__":
-    test()
+    main()
