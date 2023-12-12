adb2b402d17532b7565df33c56c57bc7aa6678d3
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-12 14:09:58 +0100

    add correct filename

--- evaluate.sh
+++ evaluate.sh
@@ -4,7 +4,7 @@ source env/bin/activate
 
 # evaluate spacy
 echo -e "[INFO:] INTIALISING EVALUATION"
-python -m spacy benchmark accuracy models/syneda_small/model-best data/spacy_formats/train.spacy --output results/syneda_small/results.json 
+python -m spacy benchmark accuracy models/syneda_small/model-best data/spacy_formats/train.spacy --output results/syneda_small/SYNEDA_results.json 
 
 echo -e "[INFO:] DONE!"
 deactivate
