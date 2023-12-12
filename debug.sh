b92730eda41dab486b977aea97a19007f187b2f3
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-12 11:16:56 +0100

    add debug bash

---
+++ debug.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# activate env
+source env/bin/activate
+
+# debug small model
+echo -e "[INFO:] DEBUGGING CONFIG ..."
+python -m spacy debug config training/configs/config_small.cfg 
+
+echo -e "[INFO:] DEBUGGING DATA ..."
+python -m spacy debug data training/configs/config_small.cfg --verbose 1
+
+
+deactivate
+echo -e "[INFO:] DONE!"
