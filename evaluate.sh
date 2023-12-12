ed9c7b16482c9dc3b5f06008323e536b92c09a42
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-12 13:57:35 +0100

    evaluate syneda small

---
+++ evaluate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source env/bin/activate
+
+# evaluate spacy
+echo -e "[INFO:] INTIALISING EVALUATION"
+python -m spacy benchmark accuracy models/syneda_small/model-best data/spacy_formats/train.spacy --output results/syneda_small/results.json 
+
+echo -e "[INFO:] DONE!"
+deactivate
