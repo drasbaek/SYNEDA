b922f296a1338088d12b3467403158892ae4e1bc
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-12 15:02:44 +0100

    change SYNEDA from train to test, add DANE + DANSK

--- evaluate.sh
+++ evaluate.sh
@@ -2,9 +2,16 @@
 
 source env/bin/activate
 
-# evaluate spacy
+## SMALL MODEL ## 
+# evaluate SYNEDA
 echo -e "[INFO:] INTIALISING EVALUATION"
-python -m spacy benchmark accuracy models/syneda_small/model-best data/spacy_formats/train.spacy --output results/syneda_small/SYNEDA_results.json 
+python -m spacy benchmark accuracy models/syneda_small/model-best data/spacy_formats/test.spacy --output results/syneda_small/SYNEDA_results.json 
+
+# evaluate DANE
+python -m spacy benchmark accuracy models/syneda_small/model-best external_data/DANE_test.spacy --output results/syneda_small/DANE_results.json
+
+# evaluate DANSK 
+python -m spacy benchmark accuracy models/syneda_small/model-best external_data/DANSK_test.spacy --output results/syneda_small/DANSK_results.json
 
 echo -e "[INFO:] DONE!"
 deactivate
