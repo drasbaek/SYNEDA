737f498dc59bcbc716e5b7cab3beb976768adf60
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-12 11:19:08 +0100

    fix verbose

--- debug.sh
+++ debug.sh
@@ -8,8 +8,7 @@ echo -e "[INFO:] DEBUGGING CONFIG ..."
 python -m spacy debug config training/configs/config_small.cfg 
 
 echo -e "[INFO:] DEBUGGING DATA ..."
-python -m spacy debug data training/configs/config_small.cfg --verbose 1
-
+python -m spacy debug data training/configs/config_small.cfg --verbose
 
 deactivate
 echo -e "[INFO:] DONE!"
