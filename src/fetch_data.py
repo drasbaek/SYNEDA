b0a16a2ef46459fcf158f486f462187c96a33a6d
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-12 14:24:35 +0100

    add fetch dane

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -36,6 +36,31 @@ def fetch_dansk():
         # save DANSK
         db.to_disk(corpus_path / f"DANSK_{p}.spacy")
 
+def fetch_dane(): 
+    try:
+        datasets = load_dataset("KennethEnevoldsen/dane_plus")
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            "Dane_Plus is not available. Check that HuggingFace is up and running, and that the dataset has been publically released.",
+        )
+    nlp = spacy.blank("da")
+    partitions = ["train", "dev", "test"]
+
+    path = pathlib.Path(__file__)
+
+    corpus_path = path.parents[1] / "external_data"
+    corpus_path.mkdir(parents=True, exist_ok=True)
+
+    for p in partitions:
+        db = DocBin()
+        for doc in [
+            Doc(nlp.vocab).from_json(dataset_row) for dataset_row in datasets[f"{p}"]
+        ]:
+            db.add(doc)
+        
+        # save DANSK
+        db.to_disk(corpus_path / f"DANE_{p}.spacy")
 
 if __name__ == "__main__":
     fetch_dansk()
+    fetch_dane()
