d67113a1c623512ca0ed66f5d0b91182fa238865
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-13 21:12:56 +0100

    rename mdls, paths in train.sh

--- train.sh
+++ train.sh
@@ -5,10 +5,10 @@ source env/bin/activate
 
 echo -e "[INFO:] INTIALISING MODEL TRAINING"
 # train SYNEDA model
-#python -m spacy train training/configs/config_SYNEDA.cfg --output ./models/SYNEDA
+#python -m spacy train training/configs/config_SYNEDA.cfg --output ./training/models/SYNEDA
 
 # train SYNEDA-DANSK
-python -m spacy train training/configs/config_SYNEDA_DANSK.cfg --output ./models/SYNEDA_DANSK
+python -m spacy train training/configs/config_SYNEDA_DANSK.cfg --output ./training/models/SYNEDA_DANSK
 
 deactivate
 echo -e "[INFO:] DONE!"
