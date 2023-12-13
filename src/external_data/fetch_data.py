1de1b867ce8cf2d7769355d923a775d3098eae6a
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-13 13:09:06 +0100

    mv file, update paths, change partitions

--- src/external_data/fetch_data.py
+++ src/external_data/fetch_data.py
@@ -1,5 +1,5 @@
 '''
-Script to fetch external data 
+Script to fetch external data (DANSK and DANE) and fix DANE. 
 
 Heavily inspired by 
 https://github.com/centre-for-humanities-computing/DaCy/blob/main/training/ner_fine_grained/fetch_assets.py 
@@ -9,9 +9,8 @@ import spacy
 from datasets import load_dataset
 from spacy.tokens import Doc, DocBin
 import pathlib
-from check_data import check_dane
 
-def fetch_dansk():
+def fetch_dansk(save_path, partitions=["train", "dev", "test"]):
     try:
         datasets = load_dataset("chcaa/DANSK")
     except FileNotFoundError:
@@ -20,12 +19,6 @@ def fetch_dansk():
         )
     
     nlp = spacy.blank("da")
-    partitions = ["train", "dev", "test"]
-
-    path = pathlib.Path(__file__)
-
-    corpus_path = path.parents[2] / "external_data"
-    corpus_path.mkdir(parents=True, exist_ok=True)
 
     for p in partitions:
         db = DocBin()
@@ -35,7 +28,7 @@ def fetch_dansk():
             db.add(doc)
         
         # save DANSK
-        db.to_disk(corpus_path / f"DANSK_{p}.spacy")
+        db.to_disk(save_path / f"{p}" /  f"DANSK_{p}.spacy")
 
 def fix_dane(dataset):
     '''
@@ -75,12 +68,12 @@ def fix_dane(dataset):
 
     return dataset
 
-def fetch_dane(): 
+def fetch_dane(save_path, partitions=["train", "dev", "test"]): 
     try:
         datasets = load_dataset("KennethEnevoldsen/dane_plus")
     except FileNotFoundError:
         raise FileNotFoundError(
-            "Dane_Plus is not available. Check that HuggingFace is up and running, and that the dataset has been publically released.",
+            "Dane+ is not available. Check that HuggingFace is up and running, and that the dataset has been publically released.",
         )
 
     path = pathlib.Path(__file__)
@@ -89,15 +82,10 @@ def fetch_dane():
     corpus_path.mkdir(parents=True, exist_ok=True)
 
     nlp = spacy.blank("da")
-    partitions = ["train", "dev", "test"]
 
-    # use fix_dane 
+    # use fix_dane (rm old MISC, LOC labls)
     for p in partitions:
         datasets[f"{p}"] = fix_dane(datasets[f"{p}"])
-
-    # check fix_dane on test dataset
-    labels = check_dane(datasets)
-    print(labels)
 
     # create DocBin 
     for p in partitions:
@@ -108,8 +96,18 @@ def fetch_dane():
             db.add(doc)
         
         # save DANE
-        db.to_disk(corpus_path / f"DANE_{p}.spacy")
+        db.to_disk(save_path / f"{p}" /  f"DANE_{p}.spacy")
+
+def main(): 
+    # define paths
+    path = pathlib.Path(__file__)
+    spacy_path = path.parents[2] / "data"
+
+    # get DANE test set 
+    fetch_dane(save_path=spacy_path, partitions=["test"])
+    
+    # get all sets from DANSK
+    fetch_dansk(save_path=spacy_path, partitions=["train", "dev", "test"])
 
 if __name__ == "__main__":
-    fetch_dansk()
-    fetch_dane()
+    main()
