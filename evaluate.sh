d80153fe9cff4e068ba23c55c754bb512ce534d7
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-14 14:09:17 +0100

    add for loop for eval

--- evaluate.sh
+++ evaluate.sh
@@ -2,27 +2,23 @@
 
 source env/bin/activate
 
-echo -e "[INFO:] INTIALISING EVALUATION"
-## SYNEDA MODEL ## 
-# evaluate SYNEDA
-python -m spacy benchmark accuracy training/models/SYNEDA/model-best data/test/SYNEDA_test.spacy --output results/SYNEDA/results_on_SYNEDA.json 
+source env/bin/activate
 
-# evaluate DANE
-python -m spacy benchmark accuracy training/models/SYNEDA/model-best data/test/DANE_test.spacy --output results/SYNEDA/results_on_DANE.json
+echo -e "[INFO:] INITIALIZING EVALUATION"
 
-# evaluate DANSK 
-python -m spacy benchmark accuracy training/models/SYNEDA/model-best data/test/DANSK_test.spacy --output results/SYNEDA/results_on_DANSK.json
+# models to evaluate
+#models=("SYNEDA" "SYNEDA_DANSK", "DANSK")
+models=("DANSK")
 
-## SYNEDA_DANSK MODEL ## 
-# evaluate SYNEDA
-python -m spacy benchmark accuracy training/models/SYNEDA_DANSK/model-best data/test/SYNEDA_test.spacy --output results/SYNEDA_DANSK/results_on_SYNEDA.json 
+# datasets to evaluate
+datasets=("SYNEDA" "DANE" "DANSK")
 
-# evaluate DANE
-python -m spacy benchmark accuracy training/models/SYNEDA_DANSK/model-best data/test/DANE_test.spacy --output results/SYNEDA_DANSK/results_on_DANE.json
-
-# evaluate DANSK 
-python -m spacy benchmark accuracy training/models/SYNEDA_DANSK/model-best data/test/DANSK_test.spacy --output results/SYNEDA_DANSK/results_on_DANSK.json
-
+for model in "${models[@]}"; do
+    for dataset in "${datasets[@]}"; do
+        # evaluate
+        python -m spacy benchmark accuracy "training/models/$model/model-best" "data/test/${dataset}_test.spacy" --output "results/$model/results_on_$dataset.json"
+    done
+done
 
 echo -e "[INFO:] DONE!"
 deactivate
