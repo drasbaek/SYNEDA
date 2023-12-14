98f327f0e1e3c12cc91796fc7910f22e1736f2ac
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-14 09:39:36 +0100

    add SYNEDA_DANSK to evaluate

--- evaluate.sh
+++ evaluate.sh
@@ -2,16 +2,27 @@
 
 source env/bin/activate
 
+echo -e "[INFO:] INTIALISING EVALUATION"
 ## SYNEDA MODEL ## 
 # evaluate SYNEDA
-echo -e "[INFO:] INTIALISING EVALUATION"
-python -m spacy benchmark accuracy training/models/syneda/model-best data/test/SYNEDA_test.spacy --output results/SYNEDA/results_on_SYNEDA.json 
+python -m spacy benchmark accuracy training/models/SYNEDA/model-best data/test/SYNEDA_test.spacy --output results/SYNEDA/results_on_SYNEDA.json 
 
 # evaluate DANE
-python -m spacy benchmark accuracy training/models/syneda/model-best data/test/DANE_test.spacy --output results/SYNEDA/results_on_DANE.json
+python -m spacy benchmark accuracy training/models/SYNEDA/model-best data/test/DANE_test.spacy --output results/SYNEDA/results_on_DANE.json
 
 # evaluate DANSK 
-python -m spacy benchmark accuracy training/models/syneda/model-best data/test/DANSK_test.spacy --output results/SYNEDA/results_on_DANSK.json
+python -m spacy benchmark accuracy training/models/SYNEDA/model-best data/test/DANSK_test.spacy --output results/SYNEDA/results_on_DANSK.json
+
+## SYNEDA_DANSK MODEL ## 
+# evaluate SYNEDA
+python -m spacy benchmark accuracy training/models/SYNEDA_DANSK/model-best data/test/SYNEDA_test.spacy --output results/SYNEDA_DANSK/results_on_SYNEDA.json 
+
+# evaluate DANE
+python -m spacy benchmark accuracy training/models/SYNEDA_DANSK/model-best data/test/DANE_test.spacy --output results/SYNEDA_DANSK/results_on_DANE.json
+
+# evaluate DANSK 
+python -m spacy benchmark accuracy training/models/SYNEDA_DANSK/model-best data/test/DANSK_test.spacy --output results/SYNEDA_DANSK/results_on_DANSK.json
+
 
 echo -e "[INFO:] DONE!"
 deactivate
