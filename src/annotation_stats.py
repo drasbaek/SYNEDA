caa2d06856a679481269a2d8b99c04a197bb3a56
Anton Drasbæk <202008161@post.au.dk>
2023-12-15 15:20:09 +0100

    restructured

--- src/annotation_stats.py
+++ src/annotation_stats.py
@@ -1,16 +1,10 @@
 import pathlib
 import pandas as pd
 
-
-def main():
-    # set paths
-    path = pathlib.Path(__file__)
-    data_path = path.parents[1] / "dbase" / "annotations" / "annotations_w_generations.xlsx"
-    outpath = path.parents[1] / "dbase" / "annotations"
-
+def annotation_errors(annotations_path, outpath):
     # load data
-    ents = pd.read_excel(data_path, sheet_name="ENTS")
-    no_ents = pd.read_excel(data_path, sheet_name="NO ENTS")
+    ents = pd.read_excel(annotations_path / "annotations_w_generations.xlsx", sheet_name="ENTS")
+    no_ents = pd.read_excel(annotations_path / "annotations_w_generations.xlsx", sheet_name="NO ENTS")
 
     # merge data
     data = pd.concat([ents, no_ents], ignore_index=True)
@@ -28,6 +22,14 @@ def main():
 
     # save to file
     error_types.to_csv(outpath / "generation_entity_errors.csv")
+
+def main():
+    # set paths
+    path = pathlib.Path(__file__)
+    annoations_path = path.parents[1] / "dbase" / "annotations"
+    outpath = path.parents[1] / "dbase" / "annotations"
+
+    annotation_errors(annoations_path, outpath)
 
 
 
